0a0c05465fb6065d08b3dcb11053fd56959edef4
Meng <mengsaoly@gmail.com>
2025-11-19 20:55:37 +0700

    OOP This KeyWords

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    int a;
+    int b ;
+    int sum;
+    public Main(int a,int b  ) {
+        this.a = a;
+        this.b =b;
+        this.sum = b + a ;
+    }
+    public void Print(){
+        System.out.println("Sum(a+b):"+sum);
+    }
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc =  new Scanner(System.in);
 
+        System.out.print("Inter a:");
+        int k = sc.nextInt();
 
+        System.out.print("Inter b:");
+        int a = sc.nextInt();
+
+        Main name = new Main(k,a);
+        name.Print();
     }
 }
